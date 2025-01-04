ab13273e5aab05684bddd266859b1335701b8cce
Biswajit0972 <workbiswajit0981@gmail.com>
2025-01-04 20:30:11 +0530

    email verification done!

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import bodyParser from "body-parser"
 import { upload} from "./middleware/multer/multer";
 import {uploadOnCloudinary} from "./utils/cloudinary/uploadOnCloud";
+import {resendEmail} from "./utils/resend/resend";
 
 
 const app = express();
@@ -12,21 +13,15 @@ const corsOptions = {
     origin: "http://localhost:3000",
     credentials: true,
 }
+
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true , limit: "10mb" }));
 app.use(cookieParser());
 
 app.post("/upload", upload.single("file"), async (req, res) => {
-     const filePath = req.file?.path
-     const url = await uploadOnCloudinary(filePath, "avatar");
-    console.log(url)
-
-
+    await resendEmail("workbiswajit0981@gmail.com");
     res.status(200).send("success");
 })
-
-
-
 
 export {app}
---
+++ src/utils/resend/resend.ts
@@ -0,0 +1,36 @@
+import { Resend } from "resend";
+import {ErrorResponse} from "../ErrorResponse/errorResponse";
+import {OTP} from "../OTP/otp";
+
+const resend = new Resend(process.env.resend_api_key);
+
+
+export const resendEmail  = async (userEmail: string) => {
+    const otp = OTP();
+    const textBody = htmlBody(otp);
+
+    const { data, error } = await resend.emails.send({
+        from: "Acme <onboarding@resend.dev>",
+        to: [userEmail],
+        subject: "hello world",
+        html: textBody,
+    });
+
+    if (error) {
+        return new ErrorResponse(error.message, "400", "Bad Request")
+    }
+
+    return data;
+}
+
+function htmlBody (otp:string) {
+  return  `<div style="font-family: Arial, sans-serif; color: #333;">
+    <h2 style="color: #007bff;">Account Verification</h2>
+    <p>Dear User,</p>
+    <p>Thank you for registering with us. To complete your account verification, please use the following One-Time Password (OTP):</p>
+    <h3 style="color: #28a745;">Your OTP: <strong> ${otp}</strong></h3>
+    <p>The OTP is valid for the next 10 minutes. Please do not share it with anyone.</p>
+    <p>If you did not request this verification, please ignore this email.</p>
+    <p>Best regards,<br>Your Company Name</p>
+  </div>`
+}
